ed9edf3b31822d4dc1bb90a1edf91c5e43f5fdb1
Evan Stoddard <evanstoddard23@gmail.com>
2015-10-28 13:46:31 -0400

    yolo

--- FallWatch WatchKit Extension/ExtensionDelegate.swift
+++ FallWatch WatchKit Extension/ExtensionDelegate.swift
@@ -18,17 +18,24 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
     func applicationDidFinishLaunching() {
         // Perform any final initialization of your application.
+        print("starting")
     }
 
     func applicationDidBecomeActive() {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("did become active")
     }
 
     func applicationWillResignActive() {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, etc.
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target:self, selector: Selector("printEverySec:"), userInfo: nil, repeats: true)
-        print("resign acitve")
+                print("will resign acitve")
+       //timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target:self, selector: Selector("printEverySec"), userInfo: nil, repeats: true)
+//        var count = 0
+//        while(true) {
+//            print("count /(count)")
+//            count++
+//        }
     }
 
 }
