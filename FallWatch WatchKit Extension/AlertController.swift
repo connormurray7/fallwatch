230b8c1deb1be30254c6cc6af2c62b85b9fef250
Andrea <aggines@umich.edu>
2015-11-16 22:29:50 -0500

    adding functionality to alert interface

---
+++ FallWatch WatchKit Extension/AlertController.swift
@@ -0,0 +1,21 @@
+//
+//  AlertController.swift
+//  FallWatch
+//
+//  Created by Andrea Gines on 11/16/15.
+//  Copyright © 2015 FallWatch. All rights reserved.
+//
+import WatchKit
+import Foundation
+import UIKit
+
+
+
+class AlertController: WKInterfaceController {
+    
+    @IBAction func cancelButton() {
+    }
+    
+    @IBAction func helpButton() {
+    }
+}
